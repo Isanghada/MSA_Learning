89746347e27a006cccb21cd2532327c466dc92ea
Isanghada <skarb4634@gmail.com>
2023-06-30 16:44:02 +0900

    init spring cloud config

--- ecommerce/src/main/java/com/example/ecommerce/EcommerceApplication.java
+++ ecommerce/src/main/java/com/example/ecommerce/EcommerceApplication.java
@@ -2,8 +2,10 @@ package com.example.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
