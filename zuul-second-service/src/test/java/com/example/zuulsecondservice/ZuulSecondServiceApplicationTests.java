780aa7a8353f4fa72437b310b2cad4bcb09c3569
Isanghada <skarb4634@gmail.com>
2023-07-03 14:27:28 +0900

    add zuul test

---
+++ zuul-second-service/src/test/java/com/example/zuulsecondservice/ZuulSecondServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zuulsecondservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulSecondServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
