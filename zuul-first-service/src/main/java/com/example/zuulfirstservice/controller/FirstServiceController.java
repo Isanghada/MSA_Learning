c1a3f506757aa2d17f033c808d63aaf38183bed7
Isanghada <skarb4634@gmail.com>
2023-07-06 15:33:02 +0900

    add LoadBalance

--- zuul-first-service/src/main/java/com/example/zuulfirstservice/controller/FirstServiceController.java
+++ zuul-first-service/src/main/java/com/example/zuulfirstservice/controller/FirstServiceController.java
@@ -1,15 +1,25 @@
 package com.example.zuulfirstservice.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/first-service/")
 @Slf4j
 public class FirstServiceController {
+    Environment env;
+    @Autowired
+    public FirstServiceController(Environment env){
+        this.env = env;
+    }
+
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to the First Service.";
@@ -21,7 +31,8 @@ public class FirstServiceController {
         return "Hello World in First Service.";
     }
     @GetMapping("/check")
-    public String check(){
-        return "Hi, there. This is a message from First Service.";
+    public String check(HttpServletRequest request){
+        log.info("Server port={}", request.getServerPort());
+        return String.format("Hi, there. This is a message from First Service on PORT %s", env.getProperty("local.server.port"));
     }
 }
