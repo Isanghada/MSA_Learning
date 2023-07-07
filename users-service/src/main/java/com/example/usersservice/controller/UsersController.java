97ba5b33ded572ea3ef42359ab95f3f0fd1fdba6
Isanghada <skarb4634@gmail.com>
2023-07-07 13:53:21 +0900

    add default code, h2 db

---
+++ users-service/src/main/java/com/example/usersservice/controller/UsersController.java
@@ -0,0 +1,26 @@
+package com.example.usersservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class UsersController {
+    private Environment env;
+    @Autowired
+    public UsersController(Environment env){
+        this.env = env;
+    }
+    @GetMapping("/health_check")
+    public String status(){
+        return "It's Working in User Service.";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return env.getProperty("greeting.message");
+    }
+}
