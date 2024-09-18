24e0a84ca797383bd961ad81f005140995f485b5
Isanghada <skarb4634@gmail.com>
2024-09-18 12:20:41 +0900

    update RestTemplate(using loadbalanced)

--- user-service/src/main/java/com/example/usersservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/usersservice/UserServiceApplication.java
@@ -3,6 +3,7 @@ package com.example.usersservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
@@ -16,6 +17,7 @@ public class UserServiceApplication {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
