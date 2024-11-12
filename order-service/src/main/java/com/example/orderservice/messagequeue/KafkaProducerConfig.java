ad990d855d00f2677e4c19462e2fd5773ee3ecc4
Isanghada <skarb4634@gmail.com>
2024-11-12 16:12:52 +0900

    update kafkaProducer (orders -> catalogs)

--- order-service/src/main/java/com/example/orderservice/messagequeue/KafkaProducerConfig.java
+++ order-service/src/main/java/com/example/orderservice/messagequeue/KafkaProducerConfig.java
@@ -18,7 +18,7 @@ public class KafkaProducerConfig {
     @Bean
     public ProducerFactory<String, String> producerFactory(){
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BATCH_SIZE_CONFIG, "127.0.0.1:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
