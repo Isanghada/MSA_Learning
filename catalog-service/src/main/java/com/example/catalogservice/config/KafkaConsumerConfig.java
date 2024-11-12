985133514e25b82cadf98a7093eb895fcfac6a11
Isanghada <skarb4634@gmail.com>
2024-11-12 13:38:18 +0900

    update kafka-consumer(order -> catalog)

---
+++ catalog-service/src/main/java/com/example/catalogservice/config/KafkaConsumerConfig.java
@@ -0,0 +1,39 @@
+package com.example.catalogservice.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaConsumerConfig {
+    // Consumer 정보를 담을 Factory 빈!
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory(){
+        Map<String, Object> properties = new HashMap<>();
+        // kafka container host
+        // - 카프카 접속 주소
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "consumerGroupId");
+        // - Json 형식이므로 Key, Value로 구성되어있고 이를 원래대로 돌리기 위해 Deserializer 지정
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(properties);
+    }
+    // 토픽에 변경사항이 있는지 체크할 수 있는 리스너 등록
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(){
+        ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory
+                = new ConcurrentKafkaListenerContainerFactory<>();
+        kafkaListenerContainerFactory.setConsumerFactory(consumerFactory());
+        return kafkaListenerContainerFactory;
+    }
+}
---
+++ catalog-service/src/main/java/com/example/catalogservice/service/KafkaConsumer.java
@@ -0,0 +1,38 @@
+package com.example.catalogservice.service;
+
+import com.example.catalogservice.entity.CatalogEntity;
+import com.example.catalogservice.repository.CatalogRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class KafkaConsumer {
+    private final CatalogRepository catalogRepository;
+    // topic에 전달된 메시지를 데이터베이스에 반영한는 작업!
+    @KafkaListener(topics="example-order-topic")
+    public void processMessage(String kafkaMessage){
+        log.info("Kafka Message: ===> "+kafkaMessage);
+
+        Map<Object, Object> map = new HashMap<>();
+        ObjectMapper mapper = new ObjectMapper();
+        try{
+            map = mapper.readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {});
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        CatalogEntity entity = catalogRepository.findByProductId((String)map.get("productId"));
+        entity.setStock(entity.getStock() - (Integer)map.get("gty"));
+
+        catalogRepository.save(entity);
+    }
+}
