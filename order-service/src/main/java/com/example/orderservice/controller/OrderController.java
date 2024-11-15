41967ec9422316407e9c839b0203998ffdc3b845
Isanghada <skarb4634@gmail.com>
2024-11-15 15:55:24 +0900

    add Distributed-Tracking(sleuth, zipkin)

--- order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -40,6 +40,7 @@ public class OrderController {
     @PostMapping("/{userId}/orders")
     public ResponseEntity<ResponseOrder> createOrder(@PathVariable("userId") String userId,
                                                      @RequestBody RequestOrder orderDetails){
+        log.info("Before add orders data");
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
@@ -61,18 +62,23 @@ public class OrderController {
         kafkaProducer.send("example-order-topic", orderDto);
         orderProducer.send("orders", orderDto);
         */
+
+        log.info("After add orders data");
+
         return ResponseEntity.status(HttpStatus.CREATED).body(returnValue);
     }
     
     // 주문 목록 요청
     @GetMapping("/{userId}/orders")
     public ResponseEntity<List<ResponseOrder>> getUserOrders(@PathVariable("userId") String userId){
+        log.info("Before retrieved orders data");
         Iterable<OrderEntity> orderList = orderService.getOrdersByUserId(userId);
 
         List<ResponseOrder> result = new ArrayList<>();
         orderList.forEach(v -> {
             result.add(new ModelMapper().map(v, ResponseOrder.class));
         });
+        log.info("After retrieved orders data");
 
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
