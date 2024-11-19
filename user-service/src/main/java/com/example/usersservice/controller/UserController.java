da837c59718dd57eeebc952dd1ac45a2b9511a28
Isanghada <skarb4634@gmail.com>
2024-11-19 09:50:11 +0900

    add micrometer

--- user-service/src/main/java/com/example/usersservice/controller/UserController.java
+++ user-service/src/main/java/com/example/usersservice/controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.usersservice.entity.UserEntity;
 import com.example.usersservice.service.UserService;
 import com.example.usersservice.vo.RequestUser;
 import com.example.usersservice.vo.ResponseUser;
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
@@ -23,6 +24,7 @@ public class UserController {
     private final Environment env;
     private final UserService userService;
     @GetMapping("/health_check")
+    @Timed(value="users.status", longTask = true)
     public String status(){
         return String.format("It's Working in User Service"
                 + ", port(local.server.port)="+ env.getProperty("local.server.port")
@@ -32,6 +34,7 @@ public class UserController {
     }
 
     @GetMapping("/welcome")
+    @Timed(value="users.welcome", longTask = true)
     public String welcome(){
         return "welcome page";
     }
